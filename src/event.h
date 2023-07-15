178f227e5b00f3615b9a2a38820ca5a5de6616da
mousebyte <ateague063@gmail.com>
2023-07-15 10:51:29 -0400

    Port event

--- src/event.h
+++ src/event.h
@@ -39,9 +39,7 @@ void client_refresh(void);
 void client_focus_refresh(void);
 void client_destroy_later(void);
 
-static inline int
-awesome_refresh(void)
-{
+static inline int awesome_refresh(void) {
     luaA_emit_refresh();
     drawin_refresh();
     client_refresh();
